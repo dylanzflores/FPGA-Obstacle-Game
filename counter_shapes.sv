9e6323a097fc9fbdc6693dbb35c52f07c9ccdd5f
flored16 <107007298+flored16@users.noreply.github.com>
2022-11-16 03:41:55 -0800

    State of obs secured

--- counter_shapes.sv
+++ counter_shapes.sv
@@ -1,32 +1,33 @@
 	module counter_shapes(input logic clk, reset, menuScreen, playerWon, playerLost,
-						  output logic [10:0] obj_position_counter, game_time);
+						  output logic [10:0] obj_position_counter, game_time,
+						  output logic levelDone);
 						  
-	always_ff @ (posedge clk, posedge reset) begin
-		if(reset) begin
-			obj_position_counter <= 0;
-			game_time <= 0;
-		end
-				// Reset level whenever user wins, loses, or goes to the menu screen.
-				else if(menuScreen == 1 | playerWon == 1 | playerLost == 1) begin
-					obj_position_counter <= 0;
-					game_time <= 0;
+		always_ff @ (posedge clk, posedge reset) begin
+			if(reset) begin
+				obj_position_counter <= 0;
+				game_time <= 0;
+			end
+			// Reset level whenever user wins, loses, or goes to the menu screen.
+			else if(menuScreen == 1 | playerWon == 1 | playerLost == 1) begin
+				obj_position_counter <= 0;
+				game_time <= 0;
+			end
+			else begin
+				if(obj_position_counter == 10'd695) begin
+					obj_position_counter <= 10'd0;
 				end
 				else begin
-					if(obj_position_counter == 10'd695)
-						obj_position_counter <= 10'd0;
-					else
-						obj_position_counter<= obj_position_counter + 10;
-						
-					// If player beats length of level user wins
-					if(game_time == 10'd200) begin
-						//dead <= 0;
-						game_time <= 0;
-					end
-					//else if(win == 1) win <= 0;
-					else begin
-						game_time <= game_time + 1;
-					end
+					obj_position_counter <= obj_position_counter + 10;
 				end
+				// If player beats length of level user wins
+				if(game_time == 10'd10000) begin
+					game_time <= 0;
+					obj_position_counter <= 0;
+				end
+				else begin
+					game_time <= game_time + 1;
+				end
+			end
 		end
 	endmodule
 	
--- levelLogic.sv
+++ levelLogic.sv
@@ -1,11 +1,11 @@
 	module levelLogic(input logic clk, reset, hit,
-					  input logic [10:0] obj_count,
+					  input logic [10:0] game_time, obj_count,
 					  output logic shapes[30:0]);
 	
 	typedef enum logic [1:0] {S0, S1, S2, S3} statetype;
 	statetype state, ns;
   
-	always_ff @ (posedge clk) begin
+	always_ff @ (posedge clk, posedge reset) begin
 		if(reset)
 			state <= S0;
 		else if(hit == 1)
@@ -17,18 +17,18 @@
 	always_comb 
 		case(state)
 		S0: begin
-				if(obj_count == 695) 
-					ns = S1;
-				else ns = S0;
+				if(game_time <= 10'd80) 
+					ns = S0;
+				else ns = S1;
 		end
 		S1: begin
-				if(obj_count == 695) 
-					ns = S2;
-				else ns = S1;
+				if(game_time <= 10'd180) 
+					ns = S1;
+				else ns = S2;
 			end
-		//S2: begin
-		//
-		//end
+		S2: begin
+			ns = S2;
+		end
 		//S3: begin
 		//
 		//end
